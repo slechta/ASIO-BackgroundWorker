916c79dd2e6dbcd4da7634e2e718d785769bf1fd
Pavel Šlechta <slechta@email.cz>
2012-04-18 14:57:06 +0200

    Two Fixes Already

---
+++ Makefile
@@ -0,0 +1,23 @@
+
+CCC = g++
+CCCFLAGS = -Wall -ggdb -std=c++0x -I. -O3
+
+OBJ =  bgworker.o
+BIN =  bgworker
+            
+LIBS-PROTOCOL = -ludt
+
+all: bgworker
+
+bgworker: $(OBJ)
+	$(CCC) $(OBJ) -o $(BIN) -lpthread -lboost_system
+
+#tells how to make an *.o object file from an *.cpp file
+%.o: %.cpp
+	$(CCC) -c $(CCCFLAGS) $< -o $@
+	
+clean::
+	rm -f bgwroker.o
+	rm -f bgworker
+	
+
---
+++ bgworker.cpp
@@ -0,0 +1,147 @@
+#include <iostream>
+#include <boost/asio.hpp>
+#include <boost/bind.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+#include <boost/thread.hpp>
+
+//-----------------------------------------------------------------------------
+
+template<typename Demuxer>
+class async_object
+{
+public:
+    typedef Demuxer demuxer_type;
+
+public:
+
+    async_object(demuxer_type& demuxer):
+        m_demuxer(demuxer)
+    {
+    }
+
+    demuxer_type& demuxer()
+    {
+        return m_demuxer;
+    }
+
+protected:
+    demuxer_type& m_demuxer;
+};
+
+//-----------------------------------------------------------------------------
+
+template<typename Demuxer>
+class background_worker:
+    public async_object<Demuxer>
+{
+public:
+
+	typedef typename background_worker<Demuxer>::demuxer_type demuxer_type;
+
+    background_worker(Demuxer& demuxer):
+        async_object<Demuxer>(demuxer),
+        m_work_demuxer(),
+        m_dummy_work(new boost::asio::io_service::work(m_work_demuxer))
+    {
+    }
+
+    ~background_worker()
+    {
+        m_dummy_work.reset();
+        if(m_work_thread)
+            m_work_thread->join();
+    }
+
+private:
+
+    template<typename Function, typename Handler>
+    class run_proc
+    {
+    public:
+
+        run_proc(demuxer_type& demuxer, Function function, Handler handler):
+            m_demuxer(demuxer),
+            m_work(demuxer),
+            m_function(function),
+            m_handler(handler)
+        {
+        }
+
+        void operator()()
+        {
+            m_function();
+            m_demuxer.post(m_handler);
+        }
+
+    private:
+        demuxer_type& m_demuxer;
+        typename demuxer_type::work m_work;
+        Function m_function;
+        Handler m_handler;
+    };
+
+public:
+
+    template<typename Function, typename Handler>
+    void async_run(Function function, Handler handler)
+    {
+        m_work_demuxer.post(run_proc<Function, Handler>(m_demuxer, function,
+            handler));
+        if(!m_work_thread)
+        {
+            m_work_thread.reset(new boost::thread(boost::bind(
+                &background_worker::work_thread_proc, this)));
+        }
+    }
+
+private:
+
+    void work_thread_proc()
+    {
+        m_work_demuxer.run();
+    }
+
+private:
+    boost::asio::io_service m_work_demuxer;
+    boost::scoped_ptr<boost::thread> m_work_thread;
+    boost::scoped_ptr<boost::asio::io_service::work> m_dummy_work;
+};
+
+//-----------------------------------------------------------------------------
+
+boost::mutex cout_mutex;
+
+void wait(int seconds)
+{
+    {
+        boost::mutex::scoped_lock lock(cout_mutex);
+        std::cout << "Wait for " << seconds << " seconds" << std::endl;
+    }
+    boost::xtime xt;
+    boost::xtime_get(&xt, boost::TIME_UTC);
+    xt.sec += seconds;
+    boost::thread::sleep(xt);
+}
+
+void wait_finished()
+{
+    {
+        boost::mutex::scoped_lock lock(cout_mutex);
+        std::cout << "Waiting finished." << std::endl;
+    }
+}
+
+//-----------------------------------------------------------------------------
+
+int main(int argc, char *argv[])
+{
+    boost::asio::io_service ios;
+    background_worker<boost::asio::io_service> bg_thread(ios);
+    bg_thread.async_run(boost::bind(wait, 1), wait_finished);
+    bg_thread.async_run(boost::bind(wait, 1), wait_finished);
+    ios.run();
+
+    bg_thread.async_run(boost::bind(wait, 1), wait_finished);
+    ios.reset();
+    ios.run();
+} 
